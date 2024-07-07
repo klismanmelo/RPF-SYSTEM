824ce4a8465317f2a90620f3b8798e8965927587
klismanmelo <klismanfdm@gmail.com>
2024-07-06 22:56:20 -0300

    Initial Commit

---
+++ RPG-SYSTEM-OB/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  RPG-SYSTEM-OB
+//
+//  Created by Klisman Ferreira on 06/07/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
